653637b58eacee9afabb9f394eeed820862f94ff
Wujinlancode <90542397+Wujinlancode@users.noreply.github.com>
2021-09-17 20:59:47 +0800

    测试提交

---
+++ 程序修改/程序修改/程序修改测试.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+fun 
+{
+	int x; struct tt* y; 
+}
+*p;
+struct tt a[4] = { 20,a + 1,15,a + 2,30,a + 3,17,a };
+main()
+{
+	int i;
+	p = a;
+	for (i = 1; i <= 2; i++)
+	{
+		printf("%d\n", p->x); p = p->y;
+	}
+}
